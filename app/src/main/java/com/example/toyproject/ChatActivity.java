f2b045fb734ed8dd20bf98a7dc7f0e45dacb9c46
junseo Lee <74139368+juns0720@users.noreply.github.com>
2024-05-05 15:37:17 +0900

    feat: 1대1 채팅 기능 일부 구현

--- app/src/main/java/com/example/toyproject/ChatActivity.java
+++ app/src/main/java/com/example/toyproject/ChatActivity.java
@@ -1,28 +1,122 @@
 package com.example.toyproject;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+//        ImageView backImage = findViewById(R.id.back);
+//
+//
+//        backImage.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//            }
+//        });
+
 
 public class ChatActivity extends AppCompatActivity {
+    private final String TAG = getClass().getSimpleName();
+
+    EditText content_et;
+    Button send_iv;
+    RecyclerView rv;
+    ChatAdapter mAdapter;
+    String chatroom = "";
+    List<ChatMsgVO> msgList = new ArrayList<>();
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat_activity);
 
+        content_et = findViewById(R.id.content_et);
+        send_iv = findViewById(R.id.send_iv);
+        mAdapter = new ChatAdapter(msgList);
+        rv = findViewById(R.id.chatRecycler);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(mAdapter);
+        chatroom = "data";
+
+        myRef = database.getReference();
 
 
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                ChatMsgVO chatMsgVO = dataSnapshot.getValue(ChatMsgVO.class);
+                msgList.add(chatMsgVO);
+                mAdapter.notifyDataSetChanged();
+                rv.scrollToPosition(msgList.size() - 1);
+            }
 
-        ImageView backImage = findViewById(R.id.back);
-        backImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) { }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) { }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) { }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+        });
+
+        send_iv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                sendMessage();
             }
         });
 
     }
+
+    private void sendMessage() {
+        String message = content_et.getText().toString().trim();
+        if (!message.isEmpty()) {
+            ChatMsgVO msgVO = new ChatMsgVO("userid", getCurrentDateTime(), message);
+            myRef.push().setValue(msgVO);
+            content_et.setText("");
+        } else {
+            Toast.makeText(this, "메시지를 입력하세요.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String getCurrentDateTime() {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return df.format(new Date());
+    }
+
 }
+
+
+
+
+
+
