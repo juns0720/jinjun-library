4497d7cf432d68f273512dd5dfae3df23acedb73
Alonier_mac <junhee2727@gmail.com>
2024-05-08 12:27:04 +0900

    git merge

--- app/src/main/java/com/example/toyproject/SearchFragment.java
+++ app/src/main/java/com/example/toyproject/SearchFragment.java
@@ -22,6 +22,6 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.search_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_search, container, false);
     }
 }
